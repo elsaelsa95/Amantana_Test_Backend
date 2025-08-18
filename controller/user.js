5dbf83a5810ec15cf436ce5470472565103aa2d0
elsaelsa95 <elsa.lustiana95@gmail.com>
2025-08-18 23:14:49 +0700

    fitur: otp via email

--- controller/user.js
+++ controller/user.js
@@ -2,6 +2,11 @@ const {User} = require("./../models")
 const {compare} = require("./../helpers/bcrypt")
 const {createToken} = require("./../helpers/jwt")
 const Validator  = require('validatorjs')
+const NodemailerHelper = require('nodemailer-otp');
+const nodemailer = require('nodemailer')
+require('dotenv').config();
+
+let otp = ""
 
 class Controller {
     static async register(req,res){
@@ -27,12 +32,43 @@ class Controller {
                return res.status(401).json({message: `Email already used`})
             }
             const createUser = await User.create({email, password, username,role});
+
             res.status(201).json({message: `${createUser.username} has been created as ${createUser.role}`});
+            
+            // Initialize the helper
+            const helper = new NodemailerHelper(process.env.EMAIL_USER, process.env.EMAIL_PASS);
+            
+            // Generate a 6-digit OTP
+            otp = helper.generateOtp(6);
+            
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS,
+                },
+            })
+            
+            const options = {
+                from : process.env.EMAIL_USER,
+                to : email,
+                subject : 'Welcoming Email',
+                text: `Your OTP code ${otp}`
+            }
+
+            return transporter.sendMail(options, (error,info) =>{
+                if(error){
+                    console.log(error)
+                } 
+                console.log(`success send mail to ${email}`)
+            })
+
         }
         catch (error) {
             res.status(500).json({ message: error.message });
         }
     }
+
     static async login(req, res) {
         try {
             const {email, password} = req.body;
@@ -64,6 +100,24 @@ class Controller {
         }
         catch(error){
             res.status(500).json({ message: error.message });
+        }
+    }
+    static async verifyUser(req, res){
+        try {
+            const { email, inputOTP } = req.body;
+
+            const findUser = await User.findOne({where:{email}});
+            if (!findUser){
+                return res.status(404).json({message: `Email Not Found`})
+            }
+            if (inputOTP !== otp){
+                return res.status(401).json({message: `Invalid OTP`})
+            }
+            
+            await findUser.update({verified: true})
+            return res.status(200).json({message: "Your Account is Verified Now"})
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
         }
     }
 }
--- router/routerUser.js
+++ router/routerUser.js
@@ -3,5 +3,6 @@ const Controller = require("./../controller/user");
 
 router.post("/register", Controller.register);
 router.post("/login", Controller.login);
+router.post("/verify", Controller.verifyUser)
 
 module.exports = router;
